942d7d1cf2947adeb249d37083303e27dfa1c6df
entaroee <entaroid@naver.com>
2016-07-27 00:16:53 -0700

    I added an sample example .py file

---
+++ 3_2_exercise.py
@@ -0,0 +1,14 @@
+try:
+	inp = raw_input("enter hours: " )
+	hours = float(inp)
+	inp = raw_input("enter rate: ")
+	rate = float(inp)
+except:
+	print "error"
+	quit()
+
+if hours <= 40:
+	pay = rate*hours
+else:
+	pay = rate*40 + (rate * 1.5 * (hours-40))
+print pay
